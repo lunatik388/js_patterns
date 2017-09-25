a5fe1b7779d2ffb181d1894e9da02d04535d0c98
ST_DOMAIN\Formago_MM <Formago_MM@st.by>
2017-09-25 20:15:57 +0300

    2

---
+++ singleton.js
@@ -0,0 +1,23 @@
+
+
+function Universe() {
+    // имеется ли экземпляр, созданный ранее?
+    if (typeof Universe.instance === 'object') {
+        return Universe.instance;
+    }
+    // создать новый экземпляр
+    this.start_time = 0;
+    this.bang = 'Big';
+    // сохранить его
+    Universe.instance = this;
+    // неявный возврат экземпляра:
+    // return this;
+}
+// проверка
+var uni = new Universe();
+var uni2 = new Universe();
+uni === uni2; // true
+
+
+// 2 способ - экземпляр в замыкании
+//внутри функции конструктора перопределяем конструктор, который возвращает var instance = this;
